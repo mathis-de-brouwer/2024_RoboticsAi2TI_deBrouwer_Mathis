0d7afc8aa955f943d2c94246cf13de6e4121f4fb
mathis-de-brouwer <mathis.de.brouwer@student.ehb.be>
2024-12-08 13:43:52 +0100

    mediapipe requirements

--- handtrack_this_pkg/setup.py
+++ handtrack_this_pkg/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),('share/handtrack_this_pkg/launch', ['launch/handtrack_this_pkg_launch_file.launch.py']),
     ],
-    install_requires=['setuptools', 'rclpy', 'opencv-python'],
+    install_requires=['setuptools', 'rclpy', 'opencv-python', 'mediapipe'],
     zip_safe=True,
     maintainer='ththis',
     maintainer_email='mathis.debrouwer@gmail.com',
